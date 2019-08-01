f19af278fcaed188c0a081ee9d52c25919504746
ASHANKARA <anantha.simha@hotmail.com>
2019-08-02 00:21:31 +0530

    comitt

--- utilities/Logging.py
+++ utilities/Logging.py
@@ -3,8 +3,8 @@ import logging
 class LogGen():
     @staticmethod
     def loggen():
-        logging.basicConfig(filename=r'C:\Users\ashankara\PycharmProjects\Pytest\logs\automations.log',
+        logging.basicConfig(filename="C://Users//ashankara//PycharmProjects//Pytest//logs//automations.log",level=logging.INFO,
                             format='%(asctime)s: %(levelname)s: %(message)s',datefmt='%m/%d/%Y %I:%M:%S %P')
         logger=logging.getLogger()
-        logger.setLevel(logging.INFO)
+
         return logger
